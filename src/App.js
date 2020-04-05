a08f2238ec8b23d619aaec4f6b895397ebe0041d
Karl O'Keeffe <github@monket.net>
2020-04-05 10:16:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const formatNumber = number => new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
+
+class App extends Component {
+  render() {
+    return (
+      <section className="product-list">
+        <input type="text" />
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Revenue</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Total</td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+      </section>
+    );
+  }
+}
+
+export default App;
